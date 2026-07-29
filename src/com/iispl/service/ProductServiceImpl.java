314ff6dcea7f56160eaf8aa8d6da935738b01cb6
yugesherukula0 <yugesherukala@gmail.com>
2026-07-29 14:29:53 +0530

    implement delete product functionality

--- src/com/iispl/service/ProductServiceImpl.java
+++ src/com/iispl/service/ProductServiceImpl.java
@@ -30,8 +30,7 @@ public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public boolean deleteProductByCode(String productCode) {
-		// TODO Auto-generated method stub
-		return false;
+		return productDao.deleteProductByCode(productCode);
 	}
 
 	@Override
